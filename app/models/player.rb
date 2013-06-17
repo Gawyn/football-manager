f98dc96a3cbd1039afe83597d2b18b21fb3720e2
Cristian Planas <me@cristianplanas.com>
2013-06-18 01:00:19 +0200

    Refactoring names constants into a module

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,25 +1,6 @@
 class Player < ActiveRecord::Base
-  NAMES = ["Antonio", "José", "Manuel", "Francisco", "Juan",
-    "David", "José Antonio",  "José Luis", "Jesús", "Javier",
-    "Carlos", "Miguel", "Pedro", "Rafael", "José Manuel",
-    "Ángel", "Daniel", "Francisco Javier", "Luis", "Fernando",
-    "Miguel Ángel", "José María", "Alejandro", "Sergio",
-    "Alberto", "Jorge", "Pablo", "Juan José", "Juan Carlos",
-    "Ramón", "Vicente", "Enrique", "Juan Antonio", "Joaquin",
-    "Andrés", "Diego", "Raúl", "Santiago", "Juan Manuel",
-    "Eduardo", "Óscar", "Álvaro", "Rubén", "Iván", "Adrián",
-    "Alfonso", "Salvador", "Roberto", "Jaime", "Francisco José"
-  ]
-  SURNAMES = [ "Abascal", "Abellán", "Acosta", "Álvarez", 
-    "Arroyo", "Bauzà", "Bayo", "Belmonte", "Beltrán", "Bou", 
-    "Cabrera", "Calzada", "Cordero", "Dalmau", "Díaz", 
-    "Durán", "Echevarría", "Egea", "Esteban", "Fernández",
-    "Fuster", "García", "Gallego", "Guzmán", "Huguet",
-    "Iborra", "Iriarte", "Jover", "Landa", "Lobo", "Losa",
-    "Macías", "Narváez", "Nogués", "Ochoa", "Otero", "Palomar",
-    "Planas", "Riera", "Rocha", "Saavedra", "Soria", "Tejera",
-    "Tirado", "Valverde", "Villalobos", "Yáñez"
-  ]
+  include Names
+
   POSITIONS = %i(goalkeeper defender midfielder striker)
   QUALITY_RANGE = 1..999
 
