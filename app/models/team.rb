dbe1a0f909715f2237fc8b287c6b39c1bdcab802
Cristian Planas <me@cristianplanas.com>
2013-06-18 01:21:36 +0200

    Generating roster

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,17 @@ class Team < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  after_create :create_roster!
+
+  private
+
+  def create_roster!
+    [[:goalkeeper,2], [:defender, 8], [:midfielder, 8], 
+      [:striker, 4]].each do |position, number| 
+      number.times do
+        players << Player.generate!(position: position)
+      end
+    end
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Team do
+  describe :hooks do
+    describe :create_roster! do
+      before do
+        @team = FactoryGirl.create :team
+      end
+
+      it "should have created 22 players" do
+        @team.players.count == 22
+      end
+    end
+  end
 end
