149baa2ba07ffb85974d6265b739181048a94ab7
Cristian Planas <me@cristianplanas.com>
2013-06-20 00:13:50 +0200

    Basic team controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 FootballManager::Application.routes.draw do
+ namespace :api, defaults: {format: 'json'} do
+    namespace :v1, defaults: { format: 'json'} do 
+      resources :teams, :only => [:index, :show]
+    end
+  end
+
   root to: "main#index"
 end
