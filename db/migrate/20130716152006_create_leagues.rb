e70cfadcad93b464c51137fbdf35741290ba4350
ray <raiarar7@gmail.com>
2013-07-29 16:51:41 +0200

    eliminate wrong migrations

--- db/migrate/20130716152006_create_leagues.rb
+++
@@ -1,9 +0,0 @@
-class CreateLeagues < ActiveRecord::Migration
-  def change
-    create_table :leagues do |t|
-      t.integer :number
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130716154522_create_league_positions.rb
+++
@@ -1,9 +0,0 @@
-class CreateLeaguePositions < ActiveRecord::Migration
-  def change
-    create_table :league_positions do |t|
-      t.integer :number
-
-      t.timestamps
-    end
-  end
-end
