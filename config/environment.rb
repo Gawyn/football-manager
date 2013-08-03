5f4d4d5912b5cd083080e1bef1c9c5de9a9708fe
Cristian Planas <me@cristianplanas.com>
2013-08-03 15:05:38 +0200

    Using APP_CONFIG constant instead of ENV

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Load APP configuration
-ENV = YAML.load_file("#{Rails.root}/config/app.yml")[Rails.env]
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")[Rails.env]
 
 # Initialize the Rails application.
 FootballManager::Application.initialize!
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['omniauth']['facebook']['id'], ENV['omniauth']['facebook']['secret']
+  provider :facebook, APP_CONFIG['omniauth']['facebook']['id'], APP_CONFIG['omniauth']['facebook']['secret']
 end
