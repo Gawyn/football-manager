f00beec8aa23371e97b66758987323616d8a6019
Cristian Planas <me@cristianplanas.com>
2013-08-03 16:31:32 +0200

    Creating a team after registration

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,4 +3,15 @@ class TeamsController < ApplicationController
     @team = Team.find params[:id]
     @players = @team.players.decorate
   end
+
+  def create
+    @team = Team.new(name: params[:team_name])
+    @team.user = current_user
+
+    if @team.save
+      redirect_to @team
+    else
+      redirect_to root_path
+    end
+  end
 end
