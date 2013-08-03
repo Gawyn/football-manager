b31ad2a4ca464194c229ca25800093d7e3e52476
Cristian Planas <me@cristianplanas.com>
2013-08-03 14:24:55 +0200

    Login through facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FootballManager::Application.routes.draw do
   resources :teams, :only => [:show]
 
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/auth/failure" => "sessions#failure"
+
   root to: "main#index"
 end
